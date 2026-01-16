46092178437416fff529a25ac007d6f9fee30f17
Ebert Alejandro Letona Abarca <221171@unamba.edu.pe>
2026-01-15 22:31:53 -0500

    Configuración inicial de entorno local: Corrección de dependencias MySQL en POM y ajuste de propiedades de base de datos

--- backend-ecommerce/src/main/java/com/epiis/app/dto/DtoCart.java
+++ backend-ecommerce/src/main/java/com/epiis/app/dto/DtoCart.java
@@ -1,8 +1,16 @@
 package com.epiis.app.dto;
 
 import com.epiis.app.generic.DtoGeneric;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DtoCart extends DtoGeneric {
-    private  String idCart;
+    private String idCart;
     private String idUser;
 }
